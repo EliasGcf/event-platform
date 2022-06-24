be7566d1819c1398a3fe08adb7b4d2bb23713522
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-06-24 16:34:17 -0300

    fix: not reload video when page change

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -28,7 +28,7 @@ export function Video(props: VideoProps) {
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId={data.lesson.videoId} />
+            <Youtube videoId={data.lesson.videoId} key={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
